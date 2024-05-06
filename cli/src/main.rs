bff6fff38bd432c5bb7e68e9e654e23921f7df7f
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-06 02:04:25 -0400

    Add multi-select and progress bar (#4)
    * Multi-select and progress bar added.
    
    * chore: Refactor CLI code and implement directory cleaning prompt and dry run option
    
    * cargo fmt
    
    * chore: Add relative path option to CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,13 +1,13 @@
 use clap::{Parser, Subcommand};
 use color_eyre::Result;
-use fs_extra::dir::get_size;
+use devclean::Cleaner;
+use dialoguer::{theme::ColorfulTheme, MultiSelect};
 use human_bytes::human_bytes;
-use humantime;
 use scanner::{
-    results::{AnalyzeTarget, AnalyzeTargets},
+    results::AnalyzeTargets,
     scanner::{get_dirty_git_repo_scanner, get_project_garbage_scanner},
 };
-use std::{io::Write, path::PathBuf};
+use std::path::PathBuf;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -24,15 +24,14 @@ struct Args {
     #[arg(long, help = "Dry Run", default_value = "false")]
     dry_run: bool,
 
-    #[arg(
-        short,
-        long,
-        help = "Clean all without Confirmation",
-        default_value = "false"
-    )]
+    #[arg(short, long, help = "Select All by Default", default_value = "false")]
     all: bool,
-    // #[arg(short, long, help = "No Need to Confirm", default_value = "false")]
-    // yes: bool,
+
+    #[arg(short, long, help = "No Need to Confirm", default_value = "false")]
+    yes: bool,
+
+    #[arg(long, help = "Display Relative Path", default_value = "true")]
+    relative: bool,
 }
 
 #[derive(Subcommand, Clone, Debug)]
@@ -45,61 +44,6 @@ enum Commands {
     },
 }
 
-struct Cleaner {
-    bytes_cleaned: u128,
-    dry_run: bool,
-    need_confirm: bool,
-}
-impl Cleaner {
-    fn new(dry_run: bool, all: bool) -> Self {
-        Cleaner {
-            bytes_cleaned: 0,
-            dry_run,
-            need_confirm: !all,
-        }
-    }
-    fn prompt_clean(&mut self, target: &AnalyzeTarget) -> Result<()> {
-        let parent = target.path.parent().unwrap();
-        let dir_name: String = target.path.file_name().unwrap().to_string_lossy().into();
-        let size = get_size(target.path.clone())?;
-        let last_modified = target.path.metadata()?.modified()?;
-        let human_modified_ago = humantime::format_duration(std::time::Duration::from_secs(
-            last_modified.elapsed()?.as_secs(),
-        ));
-        self.bytes_cleaned += size as u128;
-        println!(
-            "{}\n  └─ {} ({}) \t\t({} seconds ago)",
-            parent.display(),
-            dir_name,
-            human_bytes(size as f64),
-            human_modified_ago
-        );
-        if self.need_confirm {
-            if self.dry_run {
-                print!("(Dry Run)  ");
-            }
-            print!("Do you want to clean this directory? [y/N]:");
-        }
-        std::io::stdout().flush()?;
-        let mut input = String::new();
-        if self.need_confirm {
-            std::io::stdin().read_line(&mut input)?;
-        } else {
-            input = "y".to_string();
-        }
-        if input.trim().to_lowercase() == "y" {
-            if self.dry_run {
-                print!("(Dry Run)  ");
-            }
-            println!("Cleaning {}", target.path.display());
-            if !self.dry_run {
-                std::fs::remove_dir_all(&target.path)?;
-            }
-        }
-        Ok(())
-    }
-}
-
 fn main() -> Result<()> {
     color_eyre::install()?;
     let args = Args::parse();
@@ -109,7 +53,7 @@ fn main() -> Result<()> {
             let path = path.unwrap_or_else(|| PathBuf::from("."));
             // turn p into absolute path
             let path = std::fs::canonicalize(path)?;
-            let mut scanner = get_dirty_git_repo_scanner(path.as_path(), depth);
+            let mut scanner = get_dirty_git_repo_scanner(path.as_path(), depth, false);
             scanner.scan();
             let mut targets = vec![];
             while let Ok(target) = scanner.task_rx.recv() {
@@ -118,22 +62,42 @@ fn main() -> Result<()> {
             AnalyzeTargets(targets).to_table().printstd();
         }
         None => {
-            let path = args.path.unwrap_or_else(|| PathBuf::from("."));
-            let path = std::fs::canonicalize(path)?;
-            let mut removable_scanner = get_project_garbage_scanner(path.as_path(), args.depth);
-            removable_scanner.scan();
-            println!("Finish Scanning!");
-            let mut targets = Vec::new();
-            let mut cleaner = Cleaner::new(args.dry_run, args.all);
-            while let Ok(target) = removable_scanner.task_rx.recv() {
-                cleaner.prompt_clean(&target)?;
-                targets.push(target);
+            let mut path = args.path.unwrap_or_else(|| PathBuf::from("."));
+            if !args.relative {
+                path = std::fs::canonicalize(path)?;
             }
+            let mut removable_scanner =
+                get_project_garbage_scanner(path.as_path(), args.depth, true);
+            let mut cleaner = Cleaner::new(args.dry_run, args.all);
+            let mut target_paths = removable_scanner.scan_recursive(&path, 0);
+            target_paths.sort_by(|a, b| b.cmp(a));
+            let to_clean = if args.yes {
+                target_paths.clone()
+            } else {
+                let default_selection = if args.all {
+                    // Select all by default
+                    vec![true; target_paths.len()]
+                } else {
+                    // Select No by default
+                    vec![false; target_paths.len()]
+                };
+                let selections = MultiSelect::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Pick the directories to clean")
+                    .items(&target_paths)
+                    .defaults(&default_selection[..])
+                    .interact()?;
+                let mut to_clean = vec![];
+                for select in selections {
+                    to_clean.push(target_paths[select].clone());
+                }
+                to_clean
+            };
+            cleaner.clean_all(&to_clean)?;
             println!(
                 "Total Bytes Cleaned: {}",
                 human_bytes(cleaner.bytes_cleaned as f64)
             );
-            // AnalyzeTargets(targets).to_table().printstd();
+            AnalyzeTargets(to_clean).to_table().printstd();
         }
     }
 

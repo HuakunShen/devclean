540499992c80dd8f6dfaff8c526616a97e8a7632
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-06 21:10:25 -0400

    Remove git2 from dependency to fix build dependency error (#10)
    * Debug mac codesign
    
    * Fix MacOS Release
    
    * Add TAURI_PRIVATE_KEY
    
    * chore: Fix tauri-ci.yml format
    
    * chore: Update tauri-ci.yml to install frontend dependencies in devclean-ui directory
    
    * Fix CI
    
    * Add universal build
    
    * Add macos publish
    
    * Remove git2 to avoid openssl which could fail the published app.

--- devclean/src/predicates/languages/git.rs
+++ devclean/src/predicates/languages/git.rs
@@ -1,22 +1,27 @@
 use super::LanguagePredicate;
-use crate::predicates::Reportable;
+use crate::predicates::{
+    general::{is_git_repo, is_git_repo_clean},
+    Reportable,
+};
 use color_eyre::eyre::Result;
-use git2::{Repository, StatusOptions};
+// use git2::{Repository, StatusOptions};
 use std::path::Path;
 
 pub struct GitDirtyRepoPredicate;
 impl GitDirtyRepoPredicate {
     fn is_git_repo(&self, path: &Path) -> bool {
-        Repository::open(path).is_ok()
+        // Repository::open(path).is_ok()
+        is_git_repo(path)
     }
 
     /// no files to be commited
     fn is_git_repo_clean(&self, path: &Path) -> Result<bool> {
-        let repo = Repository::open(path)?;
-        let mut status_opts = StatusOptions::new();
-        status_opts.include_untracked(true);
-        let statuses = repo.statuses(Some(&mut status_opts))?;
-        Ok(statuses.is_empty())
+        is_git_repo_clean(path)
+        // let repo = Repository::open(path)?;
+        // let mut status_opts = StatusOptions::new();
+        // status_opts.include_untracked(true);
+        // let statuses = repo.statuses(Some(&mut status_opts))?;
+        // Ok(statuses.is_empty())
     }
 }
 
@@ -29,6 +34,18 @@ impl Reportable for GitDirtyRepoPredicate {
 
 impl LanguagePredicate for GitDirtyRepoPredicate {
     fn is_in_project(&self, path: &std::path::Path) -> bool {
-        Repository::open(path).is_ok()
+        // iterate over the parent directories to find the .git directory
+        let mut current_path = path.to_path_buf();
+        loop {
+            if current_path.join(".git").is_dir() {
+                return true;
+            }
+            if !current_path.pop() {
+                break;
+            }
+        }
+        false
+
+        // Repository::open(path).is_ok()
     }
 }

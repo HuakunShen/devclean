233e3dc03f96da8ac1ac1cc2baa4d5b7fa02cc40
Huakun Shen <huakun.shen@huakunshen.com>
2024-06-26 06:24:03 -0400

    Improve app load speed

--- devclean-ui/src/App.tsx
+++ devclean-ui/src/App.tsx
@@ -2,8 +2,13 @@ import "./App.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { HomePage } from "./components/pages/home";
+import { useEffect } from "react";
+import { getCurrent } from "@tauri-apps/api/window";
 
 function App() {
+  useEffect(() => {
+    getCurrent().show();
+  }, []);
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Toaster />
